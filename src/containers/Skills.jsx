894f3d561035ffe495972d9ab4db67558abe88ec
angieleutard <leutardangie@MacBook-Pro-de-leutard.local>
2024-08-29 17:13:09 +0200

    added table

--- src/containers/Skills.jsx
+++ src/containers/Skills.jsx
@@ -1,12 +1,13 @@
 import "../index.scss"
-import Tab from "../components/CurrentEmployees"
+import EmployeeTable from '../components/CurrentEmployees';
+
 
 function Skills() {
     return (
         <div className='skills_wrapper'>
             <h2 className="skills_title">View current employees</h2>
             <section className="currentEmployees_wrapper">
-                <Tab/>
+                <EmployeeTable/>
             </section>
         </div>
     )
